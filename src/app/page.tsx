6dc0bce712a5b920856e6dcee687f8f91fa2c813
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2024-06-02 01:03:03 +0500

    completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import HeroSection from "@/components/Herosection";
-
-
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection"; // Consistent import name
 
 export default function Home() {
   return (
-   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-     <HeroSection />
-   </main>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    
+        <HeroSection />
+   <FeaturedCourses />
+      
+    </main>
   );
 }
